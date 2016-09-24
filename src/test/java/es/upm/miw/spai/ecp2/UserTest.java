8c87b6c234a9854ebfbcd96d59a1564fb77a9794
bn0434 <bn0434@192.168.7.5>
2016-09-24 13:41:13 +0200

    Create FractionTest Create UserTest

---
+++ src/test/java/es/upm/miw/spai/ecp2/UserTest.java
@@ -0,0 +1,39 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void testUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFamilyName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFullName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitials() {
+		fail("Not yet implemented");
+	}
+
+}
