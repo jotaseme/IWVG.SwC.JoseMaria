52395a86028e79a433939140cbec96a0324253ed
bn0434 <bn0434@192.168.7.5>
2016-09-24 13:55:30 +0200

    Modificacion UserTest

--- src/test/java/es/upm/miw/spai/ecp2/UserTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/UserTest.java
@@ -2,9 +2,17 @@ package es.upm.miw.spai.ecp2;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserTest {
+	
+	private User J;
+	
+	@Before
+    public void before() {
+        J = new User(1,"Jose Maria","Moreno");
+    }
 
 	@Test
 	public void testUser() {
